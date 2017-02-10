9f21798a54e598ba53c58de1aefe5051b25c22b4
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-09 17:37:13 -0800

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglifyjs');
- 
-gulp.task('uglify', function() {
-  gulp.src('public/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
-});
-
