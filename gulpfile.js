3160f894b03c0bf20f0d7cfffd1c615f4b265160
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-11 08:59:33 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
-var beautify = require('gulp-beautify');
+var pkg = require('./package.json');
+var comment = '\/*\r\n* Wing ' + pkg.version + '\r\n* Copyright 2017, Talon Bragg\r\n* http:\/\/mrnode.tk\/\r\n* Free to use under the MIT license.\r\n* https:\/\/custommarkup.ml\/license\r\n*\/\r\n';
+var $ = require('gulp-load-plugins')();
  
-gulp.task('uglify', function() {
+gulp.task('build', function () {
+  return gulp.src(['public/js/main.min.js'])
+    .pipe($.concat('main.css'))
+    .pipe($.header(comment + '\n'))
+    .pipe($.size())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('uglify', ['build'], function() {
   gulp.src('public/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
 });
 
-    gulp.task('beautify', function() {
-    gulp.src('dist/js/main.js')
-    .pipe(beautify({indent_size: 2}))
-    .pipe(gulp.dest('dist/js'))
+gulp.task('watch', function() {
+  gulp.watch(['public/js/*.js'], ['default']);
 });
+
+gulp.task('default', ['build', 'uglify']);
