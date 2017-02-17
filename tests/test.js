0c55721777d15358758c75496b396fa31de5d457
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-17 09:47:53 -0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,18 @@
 // Open Source and free to use
 // Licensed under the MIT liscense
 //Variables
+
+//Main Test script
+var mocha = require('mocha');
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
 var fs = require('fs');
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
