e9f0b83913e50e0a708e2035cda9ab50e1bc070d
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-17 11:01:29 -0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,31 +1,18 @@
-// Mr.node
-// Copyright 2017 Talon Bragg
-// Open Source and free to use
-// Licensed under the MIT liscense
-//Variables
-//Main Test script
-var mocha = require('mocha');
+var simple = require('../dist/main.js');
+var expect = require('expect.js');
 var mrnode = require('mrnode');
-var assert = require('assert');
-if(!chai) {
-  var chai = require("chai");
-}
-var assert = require('chai').assert
-  , foo = 'bar'
-  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
 
-assert.typeOf(foo, 'string'); // without optional message
-assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
-assert.equal(foo, 'bar', 'foo equal `bar`');
-assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
-assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
-
-
-    mrnode.create = function(name, extension);
-    mrnode.delt = function(file, dest);
-    mrnode.minifyjs = function(file, dest);
-    mrnode.minifycss = function(file, dest);
-    mrnode.beautify = function(file, dest);
-    mrnode.csass = function(file, dest);
-    mrnode.renameit = function(file, newname, folderdest);
-    mrnode.createserver = function(port);
+describe("simple", function(){
+  it("Should create a file", function(){
+    mrnode.create(file, extension);
+  });
+  it("should delete a file", function(){
+    mrnode.delt(file, dest);
+  });
+  it("should minify javascript", function() {
+    mrnode.minifyjs(file, dest);
+  });
+  it("should minify css", functions() {
+     mrnode.minifycss(file, dest);
+  });
+});
