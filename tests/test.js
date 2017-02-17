15e93d484b61277565a7ca35620321ce4c267a69
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-16 16:30:37 -0800

    Add full test code

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,8 @@
-console.log('Use the "npm run" command to start a test.');
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
