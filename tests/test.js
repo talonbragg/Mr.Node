5959aac73f95aac3a0b0f95f86d5fa8e448be6c4
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-17 10:21:44 -0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -18,83 +18,51 @@ describe('#indexOf()', function() {
         fs.writeFile(name + '.' + extension, '//change the name of this file to whatever you like', function(err) {
             if (err) return console.log(err);
             console.log('File Created');
-        });
-    };
 
     // Delete Files
     mrnode.delt = function(file, dest) {
         gulp.task('delt', function() {
             return gulp.src(file)
                 .pipe(vinylPaths(del))
-                .pipe(stripDebug())
-                .pipe(gulp.dest(dest));
             if (!file) {
                 console.log('ERR! : No File Specified!');
             } else if (!dest) {
-                console.log('ERR! : No File Specified');
-            } else {
-                console.log('Success! File Deleted');
-            }
-        });
 
+        });
 
         gulp.task('default', ['clean:tmp']);
     };
     mrnode.minifyjs = function(file, dest) {
-        gulp.task('uglify', function() {
             gulp.src(file)
                 .pipe(uglify())
-                .pipe(gulp.dest(dest))
-        });
         if (!file) {
             console.log('ERR! : No File Specified!');
         } else if (!dest) {
-            console.log('ERR! : No Destination Specified');
-        } else {
-            console.log('Success! File minified!');
-        }
     };
-    mrnode.minifycss = function(file, dest) {
         gulp.task('minify', function() {
             return gulp.src([file])
                 .pipe(minifyCSS())
-                .pipe($.header(comment))
-                .pipe($.size())
                 .pipe($.concat('main.min.css'))
                 .pipe(gulp.dest(dest));
         });
         if (!file) {
             console.log('ERR! : No File Specified!');
         } else if (!dest) {
-            console.log('ERR! : No Destination Specified');
-        } else {
-            console.log('Success! File minified!');
-        }
     };
-    //Beautify Files
     mrnode.beautify = function(file, dest) {
         gulp.task('beautify', function() {
             gulp.src(file)
                 .pipe(beautify({
-                    indent_size: 2
-                }))
                 .pipe(gulp.dest(dest))
         });
         if (!file) {
             console.log('ERR! : No File Specified');
         } else if (!dest) {
-            console.log('ERR! : No Destination Specified!');
-        } else {
-            console.log('Success! File Beautified!');
-        }
     };
-    //Covert sass to css
     mrnode.csass = function(file, dest) {
         gulp.task('sass', function() {
             return gulp.src(file)
                 .pipe(sass()) // Converts Sass to CSS with gulp-sass
-                .pipe(gulp.dest(dest))
-        });
         if (!file) {
             console.log('ERR! : No File Specified!');
         }
